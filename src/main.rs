a16f2a306aea2f5a6b93b8247f1c03caf792e8b5
fcd <1352288+femnad@users.noreply.github.com>
2026-01-10 00:55:44 +0000

    add cli options for diffing upstream and default branch

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,77 @@
 use std::env;
 use std::io::Cursor;
 use std::process::Command;
-use git2::Repository;
+
+use clap::Parser;
+use git2::{Cred, Repository};
 use skim::prelude::*;
 
+const AUTH_SOCK_VAR: &str = "SSH_AUTH_SOCK";
+const DEFAULT_EDITOR: &str = "nvim";
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    auth_sock: Option<String>,
+    #[arg(short, long)]
+    remote_diff: bool,
+    #[arg(short, long)]
+    upstream_diff: bool,
+    #[arg(short, long)]
+    editor: Option<String>,
+}
+
+fn get_editor(editor: Option<String>) -> String {
+    if editor.is_some() {
+        return editor.unwrap();
+    }
+
+    if let Some(editor) = env::var_os("EDITOR") {
+        return editor.into_string().unwrap();
+    }
+
+    DEFAULT_EDITOR.to_string()
+}
+
 fn main() {
+    let args = Args::parse();
     let repo = Repository::init(".").unwrap();
-    let diff = repo.diff_index_to_workdir(None, None).unwrap();
+
+    let mut default_branch_name = String::new();
+    let diff = if args.upstream_diff {
+        let head = repo.head().unwrap();
+        let local_branch = git2::Branch::wrap(head);
+        let upstream_branch = local_branch.upstream().unwrap();
+
+        let local_tree = local_branch.get().peel_to_tree().unwrap();
+        let upstream_tree = upstream_branch.get().peel_to_tree().unwrap();
+        println!("{}", upstream_branch.name().unwrap().unwrap().to_owned());
+        repo.diff_tree_to_tree(Some(&upstream_tree), Some(&local_tree), None).unwrap()
+    } else if args.remote_diff {
+        if let Some(auth_sock) = args.auth_sock {
+            unsafe { env::set_var(AUTH_SOCK_VAR, auth_sock.as_str()); }
+        }
+
+        let mut remote = repo.find_remote("origin").unwrap();
+        let mut callbacks = git2::RemoteCallbacks::new();
+        callbacks.credentials(|_url, username_from_url, _allowed_types| {
+            let user = username_from_url.unwrap();
+            Cred::ssh_key_from_agent(&user)
+        });
+
+        remote.connect_auth(git2::Direction::Fetch, Some(callbacks), None).unwrap();
+        let default_branch_buf = remote.default_branch().unwrap();
+        default_branch_name = default_branch_buf.as_str().unwrap().to_owned();
+
+        let default_obj = repo.revparse_single(default_branch_name.as_str()).unwrap();
+        let default_tree = default_obj.peel_to_tree().unwrap();
+        let local_tree = repo.head().unwrap().peel_to_tree().unwrap();
+
+        repo.diff_tree_to_tree(Some(&default_tree), Some(&local_tree), None).unwrap()
+    } else {
+        repo.diff_index_to_workdir(None, None).unwrap()
+    };
     let mut file_names = Vec::new();
 
     for delta in diff.deltas() {
@@ -19,9 +84,15 @@ fn main() {
         return;
     }
 
+    let preview = if args.remote_diff {
+        format!("git diff {} --color=always {{}}", default_branch_name)
+    } else {
+        "git diff --color=always {}".to_string()
+    };
+
     let options = SkimOptionsBuilder::default()
         .multi(true)
-        .preview(Some("git diff --color=always {}".to_string()))
+        .preview(Some(preview))
         .build()
         .unwrap();
 
@@ -32,9 +103,8 @@ fn main() {
     if skim_out.is_abort {
     }
 
+    let editor = get_editor(args.editor);
     for item in skim_out.selected_items {
-        let editor = env::var("EDITOR").unwrap();
-        // get arg for each item
-        Command::new(editor).arg(item.output().as_ref()).status().unwrap();
+        Command::new(editor.clone()).arg(item.output().as_ref()).status().unwrap();
     }
 }
